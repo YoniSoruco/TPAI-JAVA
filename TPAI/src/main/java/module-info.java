36d079f278dba7f9a66f051afcf82002f761c0f0
JonatanSoruco <sorucojonatan@gmail.com>
2024-11-04 22:39:47 -0300

    primer com

---
+++ TPAI/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module frc.utn.edu.tpai {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens frc.utn.edu.tpai to javafx.fxml;
+    exports frc.utn.edu.tpai;
+}
